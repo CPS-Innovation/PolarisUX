0914d9ae616f7bfc78f2d3e4ee1dce252a0b6536
Christopher Hewison <chris.hewison@digital.hmrc.gov.uk>
2024-05-24 13:35:47 +0100

    design changes

--- prototype/app/assets/javascripts/tasks-and-email.js
+++ prototype/app/assets/javascripts/tasks-and-email.js
@@ -161,3 +161,14 @@ function openEmailConfirmation() {
     }
 }
 
+// Print Document
+function printDocument(divName) {
+     var printContents = document.getElementById(divName).innerHTML;
+     var originalContents = document.body.innerHTML;
+
+     document.body.innerHTML = printContents;
+
+     window.print();
+
+     document.body.innerHTML = originalContents;
+}
